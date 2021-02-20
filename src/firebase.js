725e22310113af6f9ad5f5a4e2242fa385b6a082
jackarokiason <52290328+jackarokiason@users.noreply.github.com>
2021-02-20 17:06:21 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyD33W9bJQtxY7F5AyXoZvljxX-wylcP6x0",
+   
     authDomain: "clone-6a5f0.firebaseapp.com",
     databaseURL: "https://clone-6a5f0.firebaseio.com",
     projectId: "clone-6a5f0",
